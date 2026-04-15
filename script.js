368ec32db98aa9337c1f0f0153edc94623da10ed
momentnwask <160561004+Interbanc@users.noreply.github.com>
2026-04-15 20:27:29 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<div class="input-group">
+    <input type="text" id="loginUser" placeholder="digite o i ou use a câmera">
+    <i class="fa-solid fa-camera"></i>
+</div>
+
+<div class="input-group">
+    <input type="password" id="passUser" placeholder="senha do i">
+    <i class="fa-solid fa-lock"></i>
+</div>
+
+<button type="button" onclick="validarAcesso()" class="btn-acessar">acessar</button>
